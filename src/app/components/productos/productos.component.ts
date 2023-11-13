013404707b83c07d8f2bb6c974ab8f82adc9f64c
Ricardo Alexis Lopez Arthur <19170613@itculiacan.edu.mx>
2023-11-13 16:25:38 -0700

    Agregando inputs a producto

--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./productos.component.css']
 })
 export class ProductosComponent {
-
+  listProductos: any[] = [
+    { ID: 1, nombre: 'Juguete', descripcion: 'Juguete de pokemon', precio: 299.90, cantidad: 10, creado_en: '10/10/2023 15:30'},
+    { ID: 2, nombre: 'Ropa', descripcion: 'Playera de niño', precio: 199.90, cantidad: 20, creado_en: '10/10/2023 15:40'}
+  ];
 }
