6698458ff25406a68add3f5ed8e011683231d39d
Ricardo Alexis Lopez Arthur <19170613@itculiacan.edu.mx>
2023-11-13 17:05:40 -0700

    Creando función agregarProducto y bind con inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductosComponent } from './components/productos/productos.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { ProductosComponent } from './components/productos/productos.component';
     ProductosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-productos',
@@ -10,4 +11,36 @@ export class ProductosComponent {
     { ID: 1, nombre: 'Juguete', descripcion: 'Juguete de pokemon', precio: 299.90, cantidad: 10, creado_en: '10/10/2023 15:30'},
     { ID: 2, nombre: 'Ropa', descripcion: 'Playera de niño', precio: 199.90, cantidad: 20, creado_en: '10/10/2023 15:40'}
   ];
+
+
+form: FormGroup;
+
+constructor(private fb: FormBuilder) {
+  this.form = this.fb.group({
+    id:  [''],
+    nombre: [''],
+    descripcion: [''],
+    precio: [''],
+    cantidad:  [''],
+    creado_en:  ['']
+  })
+}
+ngOnInit(): void {
+}
+
+agregarProducto(){
+
+  const producto: any = {
+    id: this.form.get('id')?.value,
+    nombre: this.form.get('nombre')?.value,
+    descripcion: this.form.get('descripcion')?.value,
+    precio: this.form.get('precio')?.value,
+    cantidad: this.form.get('cantidad')?.value,
+    creado_en: this.form.get('creado_en')?.value,
+  }
+
+}
+
+
+
 }
